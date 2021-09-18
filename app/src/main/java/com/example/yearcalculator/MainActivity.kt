a5c2aa47e13b63501634c1b3ddbc708151d92586
MeAdarshMishra <adarsh.mishra_cs19@gla.ac.in>
2021-09-18 15:00:59 +0530

    Updated

--- app/src/main/java/com/example/yearcalculator/MainActivity.kt
+++ app/src/main/java/com/example/yearcalculator/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.yearcalculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-    c
     }
+@SuppressLint("SetTextI18n")
+fun onSubmit(view:View){
+    val currentYear=Calendar.getInstance().get(Calendar.YEAR);
+    result.text="AGE: "+(currentYear-input.text.toString().toInt());
+}
 }
